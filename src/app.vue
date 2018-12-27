bc56e8d79b0db94c8b09e238640dc9d670de3dcb
Jacqueline Kolze <jkolze1@stumail.jccc.edu>
2018-12-26 19:17:41 -0600

    Add files via upload

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<div id="app">
+<h1>To Do List (With Vue.js)</h1>
+<ul class="list" v-if="items.length">
+    {{item}}
+    <span class="remove-item-button" @click="remove(index)">
+        +
+    </span>
+    </li>
+</ul>
+
+<div v-if="!items.length" class="empty-list-msg">
+empty
+</div>
+    <input type="text"
+    placeholder="New task..."
+    v-model="currentValue"
+    />
+<button @click= "add" : disabled="!cureentValue">
+    Add task
+</button>
+
+</div>
